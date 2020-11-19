2926478b44d967ce3be74ecace3fa7bfa2649c56
jinyjohny <74468597+jinyjohny@users.noreply.github.com>
2020-11-19 16:00:34 +0530

    Create .js

---
+++ Module 003/forEach/.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopIteration extends LightningElement {
+    contacts = [
+        {
+            Index: 0,
+            Id:'A1',
+            Name:'Sara Baker',
+            Email:'sara.baker@gmail.com',
+        },
+        {
+            Index: 1,
+            Id:'A2',
+            Name:'Sania Erana',
+            Email:'sania.e@gmail.com',
+        },
+        {
+            Index: 2,
+            Id:'A3',
+            Name:'Kamran Abdhulla',
+            Email:'kamran.ll@gmail.com',
+        },
+        {
+            Index: 3,
+            Id:'A4',
+            Name:'Samir Malhotra',
+            Email:'samir.m@gmail.com'
+        }
+    ]
+
+}
